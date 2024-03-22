c38e0cd6345a52eb0871e5f61d0537e034667c43
Sebiche09 <HE202263@students.ephec.be>
2024-03-22 23:12:05 +0100

    ajout de commentaire pour la compréhension

--- app/server/server.go
+++ app/server/server.go
@@ -10,17 +10,18 @@ import (
 )
 
 func main() {
-	d, err := sql.Open("mysql", dataSource())
+	d, err := sql.Open("mysql", dataSource()) //Ouvre une connexion à la DB.
+	//sql.Open() renvoie objet de type *sql.DB, fct dataSource() founrit la chaine de connexion à la db
 	if err != nil {
 		log.Fatal(err)
-	}
-	defer d.Close()
+	} // Si une erreur -> l'app s'arrete grace = log.Fatal
+	defer d.Close() //defer permet de fermer la connexion avec la DB lorsque la fonction main est terminée
 
 	// Créer une instance de moteur Gin
-	r := gin.Default()
+	r := gin.Default() //Crée une instance du moteur Gin (framework de golang)
 
 	// Définir les routes
-	setupRoutes(r, db.NewDB(d))
+	setupRoutes(r, db.NewDB(d)) //Appelle la fct setupRoutes() avec en param l'instance gin et l'instance de la DB
 
 	// CORS est activé uniquement en mode de production
 	cors := os.Getenv("profile") == "prod"
@@ -28,10 +29,10 @@ func main() {
 	// Utiliser le moteur Gin
 	addr := ":8080" // Port d'écoute
 	if os.Getenv("profile") == "prod" {
-		addr = ":80" // Port standard pour les applications web
+		addr = ":80" // Port standard pour les applications web si on est en mode production (port 80)
 	}
-	log.Printf("Serveur démarré sur %s\n", addr)
-	if err := r.Run(addr); err != nil {
+	log.Printf("Serveur démarré sur %s\n", addr) //print que le serveur est démarré
+	if err := r.Run(addr); err != nil {          //Démarre le serveur et retourne log si erreur
 		log.Fatal(err)
 	}
 }
@@ -44,7 +45,7 @@ func dataSource() string {
 		pass = os.Getenv("db_pass")
 	}
 	return "goxygen:" + pass + "@tcp(" + host + ":3306)/goxygen"
-}
+} //dataSource retourne chaine de connexion formatée poour mysql avec les valeurs host et pass
 
 // setupRoutes définit les routes de l'application web avec Gin
 func setupRoutes(r *gin.Engine, db *db.DB) {
