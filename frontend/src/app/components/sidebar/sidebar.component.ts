51b6c443018e101181a380d1bf273755d2cf46f7
Sebiche09 <HE202263@students.ephec.be>
2024-12-25 22:04:39 +0000

    0.0.6.6 - Modification du style de la sidebar

--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -4,21 +4,43 @@ import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
 import { AvatarModule } from 'primeng/avatar';
 import { StyleClassModule } from 'primeng/styleclass';
-import { Sidebar } from 'primeng/sidebar';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [SidebarModule, ButtonModule, RippleModule, AvatarModule, StyleClassModule,RouterLink],
+  imports: [SidebarModule, ButtonModule, RippleModule, AvatarModule, StyleClassModule,RouterLink, CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
 
   sidebarHovered: boolean = false;
+  mouseleaveTimeout: any;
 
   closeSidebar() {
     this.sidebarHovered = false;
+  }
+  menuItems = [
+    { label: 'COPROPRIETE(S)', icon: 'pi pi-building', link: '/displaycondominium', title: 'DASHBOARDS' },
+    { label: 'COPROPRIETAIRE(S)', icon: 'pi pi-user' },
+    { label: 'OCCUPANT(S)', icon: 'pi pi-user' },
+    { label: 'LOT(S)', icon: 'pi pi-home' },
+    { label: 'FOURNISSEUR(S)', icon: 'pi pi-truck' },
+    { label: 'Reports', icon: 'pi pi-chart-line', title: 'REPORTS' },
+    { label: 'Team', icon: 'pi pi-users' },
+    { label: 'Messages', icon: 'pi pi-comments', badge: '3' },
+    { label: 'Calendar', icon: 'pi pi-calendar' },
+    { label: 'Settings', icon: 'pi pi-cog' },
+  ];
+  onMouseLeave() {
+    if (this.mouseleaveTimeout) {
+      clearTimeout(this.mouseleaveTimeout);
+    }
+  
+    this.mouseleaveTimeout = setTimeout(() => {
+      this.sidebarHovered = false;
+    }, 200);
   }
 
 }
